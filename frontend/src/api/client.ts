903c8aa6dff379bcc29c5123e9caf85df99f9d9c
kevinqrt <kevinbrungard91@gmail.com>
2026-04-23 23:58:57 +0200

    fix: fix

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,7 +2,7 @@ import createClient from 'openapi-fetch';
 
 import type { paths } from './generated/schema';
 
-export const OLLIE_API_BASE_URL = import.meta.env.VITE_BASE_API_URL || '/api';
+export const OLLIE_API_BASE_URL = import.meta.env.VITE_BASE_API_URL;
 export const ollieApiClient = createClient<paths>({
   baseUrl: OLLIE_API_BASE_URL,
 });

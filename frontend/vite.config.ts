08a80f857a8079811813164f2cd0016f1db3baae
kevinqrt <kevinbrungard91@gmail.com>
2026-05-31 21:48:51 +0200

    fix path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: './',
   plugins: [react(), mkcert()],
   envDir: resolve(__dirname, '..'),
   server: {
